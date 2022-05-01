7c813b66d95dc3e142608972d4c8d6d5782f4b00
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-05-02 00:19:36 +0530

    Completed Registration plage 1 UI

---
+++ app/src/main/java/com/mad/divamp/location/Registration_2.java
@@ -0,0 +1,14 @@
+package com.mad.divamp.location;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration2);
+    }
+}
