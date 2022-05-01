1cbf6cc32e59f91137a391990fdade6ece5a70c4
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-05-01 23:10:44 +0530

    Completed Location log in UI

---
+++ app/src/main/java/com/mad/divamp/location/Registration.java
@@ -0,0 +1,14 @@
+package com.mad.divamp.location;
+import com.mad.divamp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
