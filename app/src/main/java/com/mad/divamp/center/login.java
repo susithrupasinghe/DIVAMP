d795a1ed9b74ccebc577eaa4e7103c878f5f0858
osanda <it20633554@my.sliit.lk>
2022-05-02 12:16:02 +0530

    vaccination center login page created

---
+++ app/src/main/java/com/mad/divamp/center/login.java
@@ -0,0 +1,14 @@
+package com.mad.divamp.center;
+import com.mad.divamp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
