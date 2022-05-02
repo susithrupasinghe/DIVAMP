4cd596400d0b0283a534714b019637f2a813c02f
osanda <it20633554@my.sliit.lk>
2022-05-02 12:17:19 +0530

    vaccination detected patient page partially created

---
+++ app/src/main/java/com/mad/divamp/center/Detected_patient.java
@@ -0,0 +1,14 @@
+package com.mad.divamp.center;
+import com.mad.divamp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detected_patient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detected_patient);
+    }
+}
