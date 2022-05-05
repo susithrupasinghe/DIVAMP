6ccbfda4f33eee8fe4786db5271b1a26feaee492
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2022-05-05 20:11:20 +0530

    File structure added

---
+++ app/src/main/java/com/mad/divamp/citizen/registration3.java
@@ -0,0 +1,16 @@
+package com.mad.divamp.citizen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mad.divamp.R;
+
+public class registration3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_citizen_registration3);
+    }
+}
