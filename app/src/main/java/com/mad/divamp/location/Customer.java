c76aedd0fb94ea60e65459375bd1a28b24092d10
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-05-05 17:00:44 +0530

    Add customer.java file

---
+++ app/src/main/java/com/mad/divamp/location/Customer.java
@@ -0,0 +1,23 @@
+package com.mad.divamp.location;
+
+public class Customer {
+
+    String name,NIC,contactNum;
+    int dateTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public String getContactNum() {
+        return contactNum;
+    }
+
+    public int getDateTime() {
+        return dateTime;
+    }
+}
