53cf9a0cdb66c138d2de3512b7f535527e9574bf
Susith Rupasinghe <susith03program@gmail.com>
2022-05-07 16:31:40 +0530

    RecyclerView added to admin

---
+++ app/src/main/java/com/mad/divamp/admin/models/cardItem.java
@@ -0,0 +1,40 @@
+package com.mad.divamp.admin.models;
+
+public class cardItem {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getRow1() {
+        return row1;
+    }
+
+    public void setRow1(String row1) {
+        this.row1 = row1;
+    }
+
+    public String getRow2() {
+        return row2;
+    }
+
+    public void setRow2(String row2) {
+        this.row2 = row2;
+    }
+
+    private String title;
+    private  String imgUrl;
+    private  String row1;
+    private  String row2;
+}
