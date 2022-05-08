366acb460b23a87d9d62b8b71d93dad909e291be
Susith Rupasinghe <susith03program@gmail.com>
2022-05-08 12:53:22 +0530

    Vaccination Center Add, read, Delete fuctions added

---
+++ app/src/main/java/com/mad/divamp/center/ui/vaccines/ManageVaccinesViewModel.java
@@ -0,0 +1,7 @@
+package com.mad.divamp.center.ui.vaccines;
+
+import androidx.lifecycle.ViewModel;
+
+public class ManageVaccinesViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
