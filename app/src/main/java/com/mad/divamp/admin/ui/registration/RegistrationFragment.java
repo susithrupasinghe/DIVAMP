3193da4fdd7d2591be9ec406cc2751158d1f7893
Susith Rupasinghe <susith03program@gmail.com>
2022-05-08 13:30:04 +0530

    Admin vaccination center registration function added

--- app/src/main/java/com/mad/divamp/admin/ui/registration/RegistrationFragment.java
+++ app/src/main/java/com/mad/divamp/admin/ui/registration/RegistrationFragment.java
@@ -12,13 +12,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mad.divamp.R;
 import com.mad.divamp.admin.ui.registration.RegistrationViewModel;
 import com.mad.divamp.databinding.FragmentRegistrationBinding;
 
+import  com.mad.divamp.admin.models.registrationModel;
+
+import es.dmoral.toasty.Toasty;
+
 public class RegistrationFragment extends Fragment {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FragmentRegistrationBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -38,5 +48,37 @@ public class RegistrationFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void insertVaccinationCenter(String email, String CenterName, String province,
+                                         String district, String password, String inchargeNIC,
+                                         String contactNo , String inchargeEmail){
+
+        registrationModel regModel = new registrationModel();
+        regModel.setEmail(email);
+        regModel.setCenterName(CenterName);
+        regModel.setProvince(province);
+        regModel.setDistrict(district);
+        regModel.setPassword(password);
+        regModel.setInchargeNIC(inchargeNIC);
+        regModel.setContactNo(contactNo);
+        regModel.setInchargeEmail(inchargeEmail);
+
+        db.collection("center").add(regModel).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+
+
+                Toasty.success(getActivity(), "Vaccination center registration successful", Toast.LENGTH_LONG, true).show();
+
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toasty.error(getActivity(), "Vaccination center registration failed", Toast.LENGTH_LONG, true).show();
+                    }
+                });
+
     }
 }
